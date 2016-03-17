e786d3280e9664ef3e925dbf273a8e35e557d1d8
Pedro Costa <dev@iampfac.com>
2016-03-17 10:56:01 +0000

    Multiple changes
    * Replaces spotlight key with L1 toggle/hold
    * Replaces thumb cluster L1 toggle/hold with L2 equivalents
    * Replaces right hand bottom right corner L1 toggle with backtick

--- keyboard/ergodox_ez/keymaps/pfac_osx_en/keymap.c
+++ keyboard/ergodox_ez/keymaps/pfac_osx_en/keymap.c
@@ -21,17 +21,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Sptlgt| Alt  |AltShf| Left | Alt  |                                       |  Up  | Down |   [  |   ]  |  L1  |
+ *   | ~L1  | Alt  |AltShf| Left | Alt  |                                       |  Up  | Down |   [  |   ]  |  `   |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
+ *                                        ,-------------.       ,---------------.
  *                                        | App  | LGui |       | Esc  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | ~L2  |       |  L2  |        |      |
  *                                 | Space| LGui |------|       |------| RGui   |Enter |
- *                                 |      |      | ~L1  |       | ~L1  |        |      |
+ *                                 |      |      | ~L2  |       | ~L2  |        |      |
  *                                 `--------------------'       `----------------------'
- *//*
- * Sptlgt: LGui + Space (for spotlight)
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
@@ -41,19 +39,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_FN1,
         CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LGUI(KC_SPC),   KC_LALT,      LALT(KC_LSFT),  KC_LEFT,KC_LALT,
+        KC_FN1,   KC_LALT,      LALT(KC_LSFT),  KC_LEFT,KC_LALT,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               MO(MDIA),
-                                               KC_SPC,KC_LGUI,KC_FN1,
+                                               KC_SPC,KC_LGUI,MO(MDIA),
         // right hand
              KC_QUOT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
              KC_FN1,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_MINS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_BSLS,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          TG(1),
+                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_GRV,
              KC_ESC,        CTL_T(KC_ESC),
              TG(MDIA),
-             KC_FN1, KC_RGUI, KC_ENT
+             MO(MDIA), KC_RGUI, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
