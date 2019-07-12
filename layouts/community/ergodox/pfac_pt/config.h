08c3f9d98954861983874fe360ecf4519eb4a0bc
Pedro Costa <dev@iampfac.com>
2019-07-12 15:52:20 +0100

    Make escape on left ctrl and tune the tap term for it

--- layouts/community/ergodox/pfac_pt/config.h
+++ layouts/community/ergodox/pfac_pt/config.h
@@ -3,11 +3,9 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-// Assume HOLD sooner (default is 200ms)
-#undef TAPPING_TERM
-#define TAPPING_TERM 100
-
-// Make TAPPING_TERM key specific
+// Make TAPPING_TERM key specific.
+// This allows overriding the TAPPING_TERM for the Ctrl/Esc key I use on
+// Caps Lock position, which I want to make a lot lower than other keys.
 #define TAPPING_TERM_PER_KEY
 
 // Assume HOLD over tap if another key is pressed along with a dual-key
@@ -17,4 +15,4 @@
 // key would sometimes fail to send the code, and some other times would send
 // it multiple times.
 #undef TAP_CODE_DELAY
-#define TAP_CODE_DELAY 5
+#define TAP_CODE_DELAY 50
